4d3c3f07b5a5dab04b8b3ddfd0a909f19a907524
Roberto Fernandes <robertofernandes199@hotmail.com>
2023-10-22 22:10:21 -0300

    Implementing domain layer

---
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,24 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+//evita que as classes Feature e News tenham o códiggos iguais..
+@MappedSuperclass
+public abstract class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String icon;
+    private String description;
+
+    public Long getId() { return id;}
+    public void setId(Long id) { this.id = id; }
+    public String getIcon() { return icon; }
+    public void setIcon(String icon) { this.icon = icon;}
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description;}
+
+}
